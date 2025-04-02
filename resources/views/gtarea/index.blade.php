bbe024c6fdeef159dea537127d7967ccbce272de
Nicolas-tec <71329970+Nicolas-tec@users.noreply.github.com>
2025-04-02 16:02:40 -0500

    edit_1

--- resources/views/gtarea/index.blade.php
+++ resources/views/gtarea/index.blade.php
@@ -1,81 +1,39 @@
-@extends('layouts.app')
-
-@section('template_title')
-    Gtareas
-@endsection
-
-@section('content')
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="card">
-                    <div class="card-header">
-                        <div style="display: flex; justify-content: space-between; align-items: center;">
-
-                            <span id="card_title">
-                                {{ __('Gtareas') }}
-                            </span>
-
-                             <div class="float-right">
-                                <a href="{{ route('gtareas.create') }}" class="btn btn-primary btn-sm float-right"  data-placement="left">
-                                  {{ __('Create New') }}
-                                </a>
-                              </div>
-                        </div>
-                    </div>
-                    @if ($message = Session::get('success'))
-                        <div class="alert alert-success m-4">
-                            <p>{{ $message }}</p>
-                        </div>
-                    @endif
-
-                    <div class="card-body bg-white">
-                        <div class="table-responsive">
-                            <table class="table table-striped table-hover">
-                                <thead class="thead">
-                                    <tr>
-                                        <th>No</th>
-                                        
-									<th >Id Tarea</th>
-									<th >D Tarea</th>
-									<th >Estatus</th>
-									<th >F Publicasion</th>
-									<th >Comentarios</th>
-									<th >Usuario</th>
-
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($gtareas as $gtarea)
-                                        <tr>
-                                            <td>{{ ++$i }}</td>
-                                            
-										<td >{{ $gtarea->id_tarea }}</td>
-										<td >{{ $gtarea->D_tarea }}</td>
-										<td >{{ $gtarea->Estatus }}</td>
-										<td >{{ $gtarea->F_publicasion }}</td>
-										<td >{{ $gtarea->Comentarios }}</td>
-										<td >{{ $gtarea->usuario }}</td>
-
-                                            <td>
-                                                <form action="{{ route('gtareas.destroy', $gtarea->id) }}" method="POST">
-                                                    <a class="btn btn-sm btn-primary " href="{{ route('gtareas.show', $gtarea->id) }}"><i class="fa fa-fw fa-eye"></i> {{ __('Show') }}</a>
-                                                    <a class="btn btn-sm btn-success" href="{{ route('gtareas.edit', $gtarea->id) }}"><i class="fa fa-fw fa-edit"></i> {{ __('Edit') }}</a>
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger btn-sm" onclick="event.preventDefault(); confirm('Are you sure to delete?') ? this.closest('form').submit() : false;"><i class="fa fa-fw fa-trash"></i> {{ __('Delete') }}</button>
-                                                </form>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-                {!! $gtareas->withQueryString()->links() !!}
-            </div>
-        </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>gestor de tareas</title>
+</head>
+<body>
+    <div class="container">
+        <h1 class="text-center p-3">Gestor de Tareas</h1>
+    <table class="table table-success table-hover">
+        <thead>
+          <tr>
+            <th scope="col">Descripcion</th>
+            <th scope="col">Estatus</th>
+            <th scope="col">Fecha de publicasion</th>
+            <th scope="col">Comentarios</th>
+            <th scope="col">Usuarios</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>4</td>
+            <td>5</td>
+            <td>6</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-@endsection
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
